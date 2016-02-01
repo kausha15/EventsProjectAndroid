f107c1f5c59e7a557e4678103105dd01409563db
kaushashah <kaushashah.cmpn@gmail.com>
2016-02-01 23:27:18 +0530

    changs

---
+++ src/main/java/com/project/event/UtilityFunctions/Logger.java
@@ -0,0 +1,13 @@
+package com.project.event.UtilityFunctions;
+
+import android.util.Log;
+
+/**
+ * Created by Dhamu on 2/1/2016.
+ */
+public class Logger {
+
+    public static void log(String tag, String message){
+        Log.d(tag,message);
+    }
+}
---
+++ src/main/java/com/project/event/fragments/EventListFragment.java
@@ -0,0 +1,55 @@
+package com.project.event.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.project.event.R;
+
+public class EventListFragment extends Fragment {
+
+    public EventListFragment() {}
+
+    public static EventListFragment newInstance(String param1, String param2) {
+        EventListFragment fragment = new EventListFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_event_list, container, false);
+
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {}
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
